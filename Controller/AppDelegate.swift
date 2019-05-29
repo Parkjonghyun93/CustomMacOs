7bb4cba219f5d7c305079ade3c5e8e7012b31f3f
Parkjonghyun93 <kltb930906@gmail.com>
2019-05-29 13:42:56 +0900

    Initial Commit

---
+++ Controller/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Controller
+//
+//  Created by 박종현 on 29/05/2019.
+//  Copyright © 2019 박종현. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Controller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Controller
+//
+//  Created by 박종현 on 29/05/2019.
+//  Copyright © 2019 박종현. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
